b65dc151472fec67247a0f7be1c7c060000a0b01
Vitalii Yatskiv <vitok2misze@gmail.com>
2024-02-03 16:48:07 +0200

    Made some changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,28 +19,33 @@ const Footer = () => {
                     <div className="footer-text"><a className="footer-text-link" href="/">Головна</a></div>
                     <div className="footer-text"><a className="footer-text-link" href="/restaurants">Ресторани</a></div>
                     <div className="footer-text"><a className="footer-text-link" href="/favourite">Улюблене</a></div>
-                    <div className="footer-text">Підписуйтесь на нас</div>
-                    <div className="footer-social-links">
-                        <div className="footer-text-link-div">
-                            <a className="footer-text-link" href="https://github.com/Vitalik2705" target="_blank"
-                               rel="noopener noreferrer">
-                                <img className="footer-text-img" src={Github} alt="GitHub"/>
-                            </a>
-                        </div>
-                        <div className="footer-text-link-div">
-                            <a className="footer-text-link" href="https://www.instagram.com/vitalikyatskiv/"
-                               target="_blank" rel="noopener noreferrer">
-                                <img className="footer-text-img" src={Instagram} alt="Instagram"/>
-                            </a>
-                        </div>
-                        <div className="footer-text-link-div">
-                            <a className="footer-text-link"
-                               href="https://www.linkedin.com/in/vitalii-yatskiv-686a63249/" target="_blank"
-                               rel="noopener noreferrer">
-                                <img className="footer-text-img" src={Linkedin} alt="LinkedIn"/>
-                            </a>
+                    <div className="footer-social-links-block">
+                        <div className="footer-text">Підписуйтесь на нас</div>
+                        <div className="footer-social-links">
+                            <div className="footer-text-link-div">
+                                <a className="footer-text-link" href="https://github.com/Vitalik2705" target="_blank"
+                                   rel="noopener noreferrer">
+                                    <img className="footer-text-img" src={Github} alt="GitHub"/>
+                                </a>
+                            </div>
+                            <div className="footer-text-link-div">
+                                <a className="footer-text-link" href="https://www.instagram.com/vitalikyatskiv/"
+                                   target="_blank" rel="noopener noreferrer">
+                                    <img className="footer-text-img" src={Instagram} alt="Instagram"/>
+                                </a>
+                            </div>
+                            <div className="footer-text-link-div">
+                                <a className="footer-text-link"
+                                   href="https://www.linkedin.com/in/vitalii-yatskiv-686a63249/" target="_blank"
+                                   rel="noopener noreferrer">
+                                    <img className="footer-text-img" src={Linkedin} alt="LinkedIn"/>
+                                </a>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className="footer-copyright">
+                    &copy; RestaurantBrowser 2024.<br/> All rights reserved.
                 </div>
             </div>
         </>
